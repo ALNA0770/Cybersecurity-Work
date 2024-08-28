96b1bcd06d2a70804d80df86e4ee4c1abe604ba0
Alijon Nazarov <89734602+ALNA0770@users.noreply.github.com>
2024-08-28 15:18:05 +0200

    Update bruteforce.py

--- Brute-Force Attack Script/bruteforce.py
+++ Brute-Force Attack Script/bruteforce.py
@@ -14,8 +14,8 @@ def attempt_extract(zf_handle, password):
 def main():
     print("[+] Beginning brute-force attack")
     # Use the absolute path to your ZIP file
-    with ZipFile(r"C:\Users\nazar\Desktop\EncryptedFilePack\enc.zip") as zf:
-        with open(r"C:\Users\nazar\Desktop\EncryptedFilePack\rockyou.txt", 'r', encoding='latin1') as f:
+    with ZipFile(r"your0file0path") as zf:
+        with open(r"your0file0path", 'r', encoding='latin1') as f:
             for line in f:
                 password = line.strip()
                 if attempt_extract(zf, password):
